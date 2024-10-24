39d36a65b12ce6033d834baae9b8f528109ca7bf
Harshitha A R <114691124+harshithaark@users.noreply.github.com>
2024-10-24 13:16:17 +0530

    Update end.js

--- Quick-quiz/end.js
+++ Quick-quiz/end.js
@@ -27,7 +27,7 @@ saveHighScore = e => {
     highScores.splice(5);
 
     localStorage.setItem('highScores', JSON.stringify(highScores));
-    window.location.assign("index.html");
+    window.location.assign("/Quick-quiz/index.html");
 
     console.log(highScores);
 }
